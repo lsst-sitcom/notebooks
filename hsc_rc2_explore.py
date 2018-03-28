c4105300f8bc642458a684367a40272611d0fa0f
Keith Bechtol <kbechtol@lsst-dev01.ncsa.illinois.edu>
2018-03-28 10:54:37 -0500

    Adding simple image visualization

--- hsc_rc2_explore.py
+++ hsc_rc2_explore.py
@@ -9,6 +9,9 @@ Note that in the example below, the registry contains many data products for whi
 do not actually exist in the data repository
 """
 
+import numpy as np
+from matplotlib import pyplot as plt
+plt.ion()
 
 from lsst.daf.persistence import Butler
 
@@ -16,10 +19,27 @@ butler = Butler('/datasets/hsc/repo/rerun/RC/w_2018_10/DM-13647/')
 
 # Get a particular calexp
 # In order to find this file, I had to look through the directory structure
+# $(pointing)/$(filter)/corr/CORR-$(visit)-$(ccd).fits
+
 #subset = butler.subset('calexp', **{'ccd':95, 'pointing':1111, 'filter':'HSC-Z', 'visit':17962})
 subset = butler.subset('calexp', **{'ccd':95, 'filter':'HSC-Z', 'visit':17962})
 calexp = butler.get('calexp', **subset.cache[0])
 src = butler.get('src', **subset.cache[0])
+
+# Image
+plt.figure(figsize=(5, 8))
+plt.imshow(calexp.getImage().array, 
+           vmin=np.percentile(calexp.getImage().array, 10), 
+           vmax=np.percentile(calexp.getImage().array, 90))
+plt.colorbar()
+plt.scatter(src.getX(), src.getY(), c='red', s=2)
+
+# Variance
+plt.figure(figsize=(5, 8))
+plt.imshow(calexp.getVariance().array,
+           vmin=np.percentile(calexp.getVariance().array, 10),
+           vmax=np.percentile(calexp.getVariance().array, 90))
+plt.colorbar()
 
 # The calexp contains many useful things, including
 # 'getCalib',
@@ -45,10 +65,12 @@ src = butler.get('src', **subset.cache[0])
 
 # Deep coadd products
 
-# Cosmos tract is 9813
+# Cosmos tract is 9813: https://jira.lsstcorp.org/browse/DM-13647
 # Again, I had to look through the file directory structure to find this
+# deepCoadd/$(filter)/$(tract)/$(patch)/calexp-$(filter)-$(tract)-$(patch).fits
 dataid = {'filter':'HSC-I', 'tract': 9813, 'patch':'4,4'}
-coadd_calexp = butler.get('deepCoadd_calexp', dataId=dataid) # Looks to contain much of the same functions as calexp
+# deep coadd calexp seems to have many of the same functions as calexp
+coadd_calexp = butler.get('deepCoadd_calexp', dataId=dataid)
 coadd_forced_src = butler.get('deepCoadd_forced_src', dataId=dataid)
 coadd_meas = butler.get('deepCoadd_meas', dataId=dataid)
 
